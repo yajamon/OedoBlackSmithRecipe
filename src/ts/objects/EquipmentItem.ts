538c49c58ed5c57dc5b0402f1633333fc081a247
yajamon <yajamon.tatsuki@gmail.com>
2014-12-14 20:41:37 +0900

    装備アイテムに鍛冶屋の価値を追加

--- src/ts/objects/EquipmentItem.ts
+++ src/ts/objects/EquipmentItem.ts
@@ -3,9 +3,11 @@
 
 class EquipmentItem extends Item {
     private spec: Spec;
+    private smith_worth: Worth;
 
-    constructor(information:Information, shop_price:Worth, spec:Spec) {
-        super(information, shop_price);
+    constructor(information:Information, shop_worth:Worth, spec:Spec, smith_worth:Worth) {
+        super(information, shop_worth);
         this.spec = spec;
+        this.smith_worth = smith_worth;
     }
 }
